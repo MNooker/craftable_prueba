48936acbf60828df685b0b00a4b336e659d4ddfb
Nooker _ <nooker106@gmail.com>
2020-08-17 14:31:13 -0600

    version 1.01

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
+Route::get('/hola', function () {
+    return view('hola');
+});
 
 /* Auto-generated admin routes */
 Route::middleware(['auth:' . config('admin-auth.defaults.guard'), 'admin'])->group(static function () {
